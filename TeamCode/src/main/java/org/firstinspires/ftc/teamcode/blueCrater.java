4b2ae06dbff5ce8a8fdee14df8c7a9dd3f8b08b3
williamtrang <32171517+williamtrang@users.noreply.github.com>
2018-12-02 12:37:39 -0800

    Update blueCrater.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueCrater.java
@@ -23,14 +23,17 @@ public class blueCrater extends autoMethods {
         encoderDrive(1,12,12);
         //lowerSlide();
         sleep(500);
+        //turn towards wall and drive to depot
         turnLeft(42,0.5);
         encoderDrive(1,36,36);
         turnLeft(80,0.5);
         encoderDrive(1, 75, 75);
+        //drop team makrer
         robot.marker.setPosition(0);
         sleep(1500);
         robot.marker.setPosition(0.99);
         sleep(600);
+        //turn and drive to crater
         turnLeft(100,0.5);
         encoderDrive(1,-84,-84);
     }
