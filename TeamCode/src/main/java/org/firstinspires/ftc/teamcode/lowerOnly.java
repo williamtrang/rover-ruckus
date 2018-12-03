46f3b8af6e75d49a2b08e2bb38dd396be9193cb6
FTC 9873 <shadowskarmory17@gmail.com>
2018-12-02 18:27:33 -0800

    added lower only autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lowerOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lowerOnly.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "blue crater", group = "test")
-public class blueCrater extends autoMethods {
+@Autonomous (name = "lower robot only", group = "test")
+public class lowerOnly extends autoMethods{
     @Override
     public void runOpMode(){
         robot.init(hardwareMap);
@@ -18,26 +18,9 @@ public class blueCrater extends autoMethods {
 
         waitForStart();
 
-        //lower robot from lander and drive back
+        //lower from lander
         lowerRobot();
-        encoderDrive(1,12,12);
-        //lowerSlide();
-        sleep(500);
-
-        //turn towards wall and drive to depot
-        turnLeft(42,0.5);
-        encoderDrive(1,36,36);
-        turnLeft(80,0.5);
-        encoderDrive(1, 75, 75);
-
-        //drop team marker
-        robot.marker.setPosition(OPEN);
-        sleep(1500);
-        robot.marker.setPosition(CLOSED);
-        sleep(600);
-
-        //turn and drive to crater
-        turnLeft(100,0.5);
-        encoderDrive(1,-84,-84);
+        encoderDrive(1,4,4);
+        lowerSlide();
     }
 }
